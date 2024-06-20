8892d90fe80a51a951ee12e8942aeb96e9759ed9
Benjamin Green <bkingsleygreen@gmail.com>
2024-06-20 23:08:32 +0100

    Initial commit

---
+++ src/AviationWeather.CSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿if (args.Length == 0)
+{
+    Console.WriteLine("Please provide an ICAO code.");
+    return;
+}
+var icao = args[0].ToUpper();
+if (string.IsNullOrWhiteSpace(icao))
+{
+    Console.WriteLine("Invalid ICAO code.");
+    return;
+}
+var url = $"https://aviationweather.gov/api/data/metar?ids={icao}&format=raw&taf=false";
+var client = new HttpClient();
+var response = await client.GetAsync(url);
+var content = await response.Content.ReadAsStringAsync();
+Console.WriteLine(content);
